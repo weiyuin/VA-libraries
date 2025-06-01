90956a446aee642ecc9cafabf148b9e71ee70ba7
Tùng Nguyễn Công <lecongphat133@gmail.com>
2025-06-01 23:04:38 +0700

    Cập nhật 1

--- redirectstdout.py
+++ redirectstdout.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import sys
 import ctypes
 import io
@@ -37,43 +36,3 @@ def redirectPrint():
 
 def resetPrint():
     sys.stdout = myRedirect.__console__
-=======
-import sys
-import ctypes
-import io
-from threading import Lock
-lock = Lock()
-
-
-global dllFunprint
-global myRedirect
-
-
-class __redirection__(io.TextIOBase):
-    def __init__(self):
-        self.buff=''
-        self.__console__=sys.stdout
-        
-    def write(self, output_stream):
-        global dllFunprint
-        with lock:
-            self.buff = output_stream
-            if type(self.buff).__name__ == 'str':
-                self.buff = self.buff.encode('utf-8')
-            dllFunprint(self.buff)
-
-    def reset(self):
-        sys.stdout = self.__console__
-        
-def redirectPrint():
-    global dllFunprint
-    global myRedirect
-    dll = ctypes.cdll.LoadLibrary("RedirectPythonPrint.dll")
-    dllFunprint = dll.pyPrint
-    dllFunprint.argtypes = [ctypes.c_char_p]
-    myRedirect  = __redirection__()
-    sys.stdout = myRedirect
-
-def resetPrint():
-    sys.stdout = myRedirect.__console__
->>>>>>> c64d729fd419c4ce1675d7be701b9a882322c69d
